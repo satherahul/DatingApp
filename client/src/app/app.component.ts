25cc04368ea5cb234b8eaa695e418aa8684c0f39
rahul sathe <rahulsathe3720@gmail.com>
2025-05-26 00:54:39 +0530

    adding photo and member cads

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -21,6 +21,7 @@ export class AppComponent implements OnInit{
   }
   setCurrentUser(){
     const user: User = JSON.parse(localStorage.getItem('user'));
+   this.accountService.setCurrentUser(user);
   }
  
 }
